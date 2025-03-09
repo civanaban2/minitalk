bffc42638b9132e537f3f5ac2d8fcf6277ab3d74
Civan Rumet Arı <civanrumet@hotmail.com>
2025-03-09 20:04:28 +0300

    Update

--- src/client.c
+++ src/client.c
@@ -1,27 +1,67 @@
 #include "client.h"
 
-char a = 0b00000000;
+int		total = 0;
 
-void handler(int signum)
+void	sig_handler(int signum)
 {
 	if (signum == SIGUSR1)
 	{
-		a = a << 1 | 0b00000001;
-	}
-	else if (signum == SIGUSR2)
-	{
-		a = a << 1;
+		total++;
 	}
 }
 
-int main(/* int argc, char const *argv[] */)
+void	send_byte(int pid, uint8_t byte)
 {
-	ft_printf("PID: %d\n", getpid());
-	signal(SIGUSR1, handler);
-	signal(SIGUSR2, handler);
-	while (1) {
-		ft_printf("%d\n", a);
-		ft_printf("%c\n", a);
-		sleep(1);
+	int i;
+
+	i = 0;
+	while (i < 8)
+	{
+		if (byte & (1 << i))
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		i++;
+		pause();
 	}
+}
+
+void send_int(int pid, uint32_t size)
+{
+	int i;
+
+	i = 0;
+	while (i < 32)
+	{
+		if (size & (1 << i))
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		i++;
+		pause();
+	}
+}
+
+int main(int argc, char **argv)
+{
+	int size;
+	int pid;
+
+	signal(SIGUSR1, sig_handler);
+	if (argc != 3)
+	{
+		ft_printf("Usage: %s <server-pid> <message>\n", argv[0]);
+		return 1;
+	}
+	size = ft_strlen(argv[2]);
+	pid = atoi(argv[1]);
+
+	send_int(pid, size);
+	while (*argv[2])
+	{
+		send_byte(pid, *argv[2]);
+		argv[2]++;
+	}
+
+	return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,65 +1,73 @@
 #include "server.h"
 
-
-void signal_handler(int signum, siginfo_t *info, void *ptr)
+void	set_bit(uint8_t *message, uint8_t bit, int signum)
 {
-	static unsigned int size; 
-	static uint8_t bit_count = 0;
-	static uint8_t byte = 0;
-	static uint8_t status;
-	static char *message;
-	static unsigned int i;
+	if (signum == SIGUSR1)
+		*message |= (1 << bit);
+	else if (signum == SIGUSR2)
+		*message &= ~(1 << bit);
+}
 
-	if (status = 0x00)
+void	set_int(uint32_t *size, uint8_t bit, int signum)
+{
+	if (signum == SIGUSR1)
+		*size |= (1 << bit);
+	else if (signum == SIGUSR2)
+		*size &= ~(1 << bit);
+}
+
+void	signal_handler(int signum, siginfo_t *info, void *context)
+{
+	static uint32_t				size;
+	static uint8_t				*message;
+	static unsigned long long	bit;
+	static uint8_t				status;
+
+	(void)context;
+	if (status == 0x00)
 	{
-		if (signum == SIGUSR1)
+		if (bit < 32)
 		{
-			size |= 0x01 << bit_count;
+			set_int(&size, bit, signum);
+			bit++;
 		}
-		bit_count++;
-		if (bit_count == 32)
+		if (bit == 32)
 		{
+			message = (uint8_t *)malloc(size + 1);
+			bit = 0;
 			status = 0x01;
-			bit_count = 0;
-			message = malloc(size);
 		}
 		kill(info->si_pid, SIGUSR1);
 	}
 	else if (status == 0x01)
 	{
-		if (signum == SIGUSR1)
+		if (bit < 8 * size)
 		{
-			byte |= 0x01 << bit_count;
+			set_bit(&message[bit / 8], bit % 8, signum);
+			bit++;
 		}
-		bit_count++;
-		if (bit_count == 8)
+		if (bit == 8 * size)
 		{
-			message[i] = byte;
-			i++;
-			if (i == size)
-			{
-				status = 0x00;
-				i = 0;
-				write(1, message, size);
-				free(message);
-			}
-			bit_count = 0;
+			message[size] = '\0';
+			ft_printf("Received message: %s\n", message);
+			free(message);
+			bit = 0;
+			status = 0x00;
 		}
+		kill(info->si_pid, SIGUSR1);
 	}
 }
 
-int main()
+int	main(void)
 {
-	struct sigaction act;
+	struct sigaction	sa;
 
-	act.sa_sigaction = signal_handler;
-	act.sa_flags = SA_SIGINFO;
-	sigaction(SIGUSR1, &act, NULL);
-	sigaction(SIGUSR2, &act, NULL);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = signal_handler;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	ft_printf("Server PID: %d\n", getpid());
 	while (1)
-	{
-		pause();
-	}
+		pause ();
 	return (0);
-	
 }
